57077badd0ad665c85192be299b7d3ef7f5b4620
mh <mh@dummy.ne.jp>
2014-04-12 14:47:02 +0900

    PostgreSQL設定

--- app/controllers/QuestionController.scala
+++ app/controllers/QuestionController.scala
@@ -6,6 +6,7 @@ import models.Questions
 import play.api.db.DB
 import play.api.Play.current
 
+import scala.util.Random
 import scala.slick.session.Database
 
 object QuestionController extends QuestionController {
@@ -13,10 +14,12 @@ object QuestionController extends QuestionController {
 
 class QuestionController extends Controller {
 
-  lazy val database = Database.forDataSource(DB.getDataSource())
+  val database = Database.forDataSource(DB.getDataSource())
 
   def showQuestion() = Action { implicit request =>
     database withSession {
+      val r = new Random
+      val questionId = r.nextInt(4) + 1
       val question = Questions.getByQuestionId(1)
       Ok(views.html.question.showQuestion("問題です！", question.get))
     }
